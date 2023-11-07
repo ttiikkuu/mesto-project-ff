857950bb51513f8844de0b892af6a50b0118339b
Tiku <tiku@tiku.ru>
2023-11-07 22:30:39 +0300

    работа 5 на первую проверку

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/ttiikkuu/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+	
 // @todo: DOM узлы
+
+const cardsContainer = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
 
+function cards(callback) {
+	
+	for (let i = 0; i < initialCards.length; i++) {
+		
+		const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+			  
+		cardElement.querySelector('.card__image').src = initialCards[i].link;
+		
+		cardElement.querySelector('.card__title').textContent = initialCards[i].name;
+
+        cardElement.querySelector('.card__delete-button').addEventListener('click', callback);
+
+		cardsContainer.append(cardElement); 
+
+	}
+
+}
+
 // @todo: Функция удаления карточки
 
+function deleteCard() {
+    const card = document.querySelector('.card__delete-button').closest('.card');
+    card.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+cards(deleteCard);
+
+
